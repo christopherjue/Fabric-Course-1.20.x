56c91e15170bbc77357047bc7f2b9dc659c25145
Christopherjue <cj@phy6.net>
2023-11-03 13:39:20 -0400

    Initial commit

---
+++ src/main/java/net/christopher/mccourse/MCCourseMod.java
@@ -0,0 +1,20 @@
+package net.christopher.mccourse;
+
+import net.christopher.mccourse.block.ModBlocks;
+import net.christopher.mccourse.item.ModItems;
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MCCourseMod implements ModInitializer {
+	public static final String MOD_ID = "mccourse";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
+	}
+}
